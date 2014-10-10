be7c999069b1f21939a7ac1a4ed6c7a7036f1e80
Masato Ikeda <masato.ikeda@gmail.com>
2014-10-11 01:28:10 +0900

    Cosmetic

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,13 +1,11 @@
 class RootController < ApplicationController
   def index
-    @total_unreads_count = 10
     @feeds = Feed.all
 
     @entries = Entry.newer
   end
 
   def show
-    @total_unreads_count = 10
     @feeds = Feed.all
 
     @feed = Feed.find_by uid: params[:uid]

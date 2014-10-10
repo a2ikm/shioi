9ae6e6d2cdb84184c0b6c62bc641a05e31b50d43
Masato Ikeda <masato.ikeda@gmail.com>
2014-10-10 23:47:30 +0900

    Fetch and load entries with `Feed#fetch!`

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -4,4 +4,34 @@ class Feed < ActiveRecord::Base
   validates :feed_url,  presence: true
 
   has_many :entries,  inverse_of: :feed, dependent: :destroy
+
+  def self.create_or_update!(feed_url)
+    feed = Feedjira::Feed.fetch_and_parse feed_url
+
+    where(feed_url: feed_url).first_or_initialize.tap do |record|
+      record.title  = feed.title
+      record.url    = feed.url
+      record.save!
+    end
+  end
+
+  def fetch!
+    feed = Feedjira::Feed.fetch_and_parse feed_url
+
+    update!({
+      title:  feed.title,
+      url:    feed.url,
+    })
+
+    feed.entries.each do |feed_entry|
+      entries.where(url: feed_entry.url).first_or_create! do |entry|
+        entry.title = feed_entry.title
+        entry.created_at = feed_entry.published.to_time
+        entry.updated_at = feed_entry.updated.to_time
+        entry.body = feed_entry.content
+      end
+    end
+
+    self
+  end
 end

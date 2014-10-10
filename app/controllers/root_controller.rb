4629a1de26ae6131dd30757a56d6ac214b7ccc44
Masato Ikeda <masato.ikeda@gmail.com>
2014-10-11 00:54:00 +0900

    Show feeds stored in database

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -2,16 +2,7 @@ class RootController < ApplicationController
   def index
     @total_unreads_count = 10
 
-    @subscriptions = (1..10).map do |i|
-      Hashie::Mash.new({
-        unreads_count: rand(1..100),
-        feed: {
-          name: Faker::Lorem.word,
-          url: Faker::Internet.http_url, 
-        }
-      })
-    end
-
+    @feeds = Feed.all
     @entries = Entry.newer
   end
 end
